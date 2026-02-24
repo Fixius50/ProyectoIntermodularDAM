2c5968ac4ea3d79ce246972dd56fdf86d720b1a1
Fixius50 <robertomonederoalonso0@gmail.com>
2026-02-24 16:17:20 +0100

    asd

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    define: {
+        // Necesario para algunas librerías de React Native
+        global: 'window',
+    },
     resolve: {
         alias: {
             'react-native': 'react-native-web',
         },
+        extensions: ['.web.js', '.web.jsx', '.web.ts', '.web.tsx', '.mjs', '.js', '.jsx', '.json', '.cjs', '.ts', '.tsx']
     },
     optimizeDeps: {
         esbuildOptions: {
